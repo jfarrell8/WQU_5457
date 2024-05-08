1a520b4237ae95e23d57eb580e73fb854567fdc3
John Farrell <johndfarrell@gmail.com>
2024-05-08 14:12:46 -0500

    updated wealth_index

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,11 @@
 import configparser
 import os
+import pandas as pd
 import numpy as np
 import random
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from dateutil.relativedelta import relativedelta
 
 
 def load_config():
@@ -30,6 +32,13 @@ def set_seeds(seed_state):
 
 
 def plot_wealth_index(portfolio_returns, initial_amount, timestamp, model_version):
+    # need to insert a row of 0s at the beginning so each line starts at the initial amount
+    first_date = portfolio_returns.index[0]
+    previous_month_end = first_date - relativedelta(months=1)
+    new_row_values = [0] * len(portfolio_returns.columns)
+    portfolio_returns = pd.concat([pd.DataFrame([new_row_values], columns=portfolio_returns.columns, index=[previous_month_end]), portfolio_returns], axis=0)
+
+    # now calculate cumulative rets and wealth index
     cumulative_rets = (1 + portfolio_returns).cumprod()
     wealth_index = initial_amount * cumulative_rets
 
